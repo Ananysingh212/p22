e120fa7193fc137f91cb7c8f9a32c097c501733f
Ananysingh212 <78842855+Ananysingh212@users.noreply.github.com>
2021-05-15 13:52:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,11 +11,11 @@ const Body = Matter.Body;
 
 function preload()
 {
-	starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
+	starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
 	//load animation for fairy here
-	fairyImg = loadImage("images/fairyImage1.png","images/fairyImage2.png");
-	music = loadSound("sound/JoyMusic.mp3");
+	fairyImg = loadImage("fairyImage1.png","fairyImage2.png");
+	music = loadSound("JoyMusic.mp3");
 }
 
 function setup() {
